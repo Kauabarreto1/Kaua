2de466f6b9e889e258872b4e62d38dae619b560a
Kauabarreto1 <166553193+Kauabarreto1@users.noreply.github.com>
2024-04-16 16:35:15 -0300

    Add files via upload

---
+++ área e perímetro.py
@@ -0,0 +1,5 @@
+base = float(input("Digite a base do retângulo: "))
+altura = float(input("Digite a altura do retângulo: "))
+area = (altura*base)
+perimetro = (altura+base)
+print ("A área do retângulo é",area,"O perímetro do retângulo é",perimetro)
