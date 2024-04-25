4671e1ce93186b33770a4cfd6c1b1aba4c623f03
Kauabarreto1 <166553193+Kauabarreto1@users.noreply.github.com>
2024-04-25 15:36:46 -0300

    Add files via upload

---
+++ calculadora de impostos.py
@@ -0,0 +1,5 @@
+valor = float(input("Qual o valor? R$"))
+juros = float(input("Qual a taxa de juros? "))
+tempo = float(input("Quanto tempo acumulando? "))
+total = valor * juros * tempo
+print (total)
